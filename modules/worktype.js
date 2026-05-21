65ebb8499ace9549748c74e0b3a9775361cb18ca
shaan.403 <alinshan2255@gmail.com>
2026-05-21 14:55:43 +0530

    Fix worktype command to reply correctly for both fromMe and sudo users

--- modules/worktype.js
+++ modules/worktype.js
@@ -1,14 +1,21 @@
 addCommand({ pattern: "^worktype ?(.*)", access: "sudo", desc: "_Change the working type of the bot._", usage: global.handlers[0] + "worktype <public || private>" }, async (msg, match, sock, rawMessage) => {
+    const reply = async (text) => {
+        if (msg.key.fromMe) {
+            return await sock.sendMessage(msg.key.remoteJid, { text, edit: msg.key });
+        } else {
+            return await sock.sendMessage(msg.key.remoteJid, { text }, { quoted: rawMessage.messages[0] });
+        }
+    };
 
-    var worktype = match[1];
+    var worktype = match[1]?.trim().toLowerCase();
     if (!worktype) {
-        return await sock.sendMessage(msg.key.remoteJid, { text: "_Please specify the working type of the bot._\n\n_Bot is currently set to_ " + global.database.worktype + "._", edit: msg.key });
+        return await reply("_Please specify the working type of the bot._\n\n_Bot is currently set to_ *" + global.database.worktype + "*._");
     }
 
-    if (worktype == "public" || worktype == "private") {
+    if (worktype === "public" || worktype === "private") {
         global.database.worktype = worktype;
-        return await sock.sendMessage(msg.key.remoteJid, { text: "_The working type of the bot has been changed to " + worktype + "_", edit: msg.key });
+        return await reply("_The working type of the bot has been changed to_ *" + worktype + "*._");
     } else {
-        return await sock.sendMessage(msg.key.remoteJid, { text: "_Invalid working type. Please use 'public' or 'private'._", edit: msg.key });
+        return await reply("_Invalid working type. Please use 'public' or 'private'._");
     }
 })
